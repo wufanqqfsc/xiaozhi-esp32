7034a1956e852cfc75e3ed2bed4a290ecb55cc0a
wufanqqfsc <fanshichao@126.com>
2026-06-29 11:06:03 +0800

    add gif

---
+++ doc/lib/gifenc-browser.js
@@ -0,0 +1,4 @@
+/* gifenc CJS → 浏览器全局 gifenc */
+var gifenc = {};
+var exports = gifenc;
+var module = { exports: gifenc };
---
+++ doc/lib/gifenc.mjs
@@ -0,0 +1,3 @@
+/* esm.sh - gifenc@1.0.3 */
+export * from "/gifenc@1.0.3/es2022/gifenc.mjs";
+export { default } from "/gifenc@1.0.3/es2022/gifenc.mjs";
